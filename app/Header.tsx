c976903ba6915026d7415a9356f8493f51a0ac82
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-02-07 21:52:40 +0100

    Header component created

---
+++ app/Header.tsx
@@ -0,0 +1,10 @@
+import { MdAddBox } from 'react-icons/md';
+
+export default function Header(){
+
+    return(
+        <>
+            <MdAddBox color='#E9AFA3' size='70' className='my-5 mx-5 cursor-pointer'/>
+        </>
+    )
+}
